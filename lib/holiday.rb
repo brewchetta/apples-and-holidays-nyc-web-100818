529b9880c161c631f1b221559556584eeb9eb5d2
Chett Tiller <chett.tiller@gmail.com>
2018-09-24 16:38:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |k,v|
       print "#{k.to_s.split(" ").each{|x| x.capitalize!}.join}: "
       v.each do |x|
-        x != v.last ? print "#{x} ," : puts "#{x}"
+        (x != v.last) ? print "#{x} ," : puts "#{x}"
     end
   end
 end
